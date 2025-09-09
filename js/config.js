eabf9fe2405cbf623fcfd322a2d9c07d5bbd7bbc
Walter Magill <wallymagill15@gmail.com>
2025-09-09 19:55:36 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_45rnGokY7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7e4fgau2rukb13sqcpjeki00l7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
